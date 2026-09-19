7199f31e59e487a256af40281cf413b0ee84e937
mins2269 <146416296+mins2269@users.noreply.github.com>
2026-09-19 13:39:22 +0900

    Update WaterShortage.java

--- codingtest/week2/src/WaterShortage.java
+++ codingtest/week2/src/WaterShortage.java
@@ -12,9 +12,49 @@ public class Water {
         System.out.println("usage: ");
         usage = sc.nextInt();
         System.out.println(Arrays.toString(change));
+        System.out.println("결과: " + solution(storage, usage, change));
+        sc.close();
+    }
+
+        public static int solution(int storage, int usage, int[] change) {
+            int total_usage = 0;
+            for(int i=0; i<change.length; i++){
+                // 물 사용량 변동량
+                // 매달 물 사용량은 소수점 이하를 버린 정수로 계산
+                // usage = (int) (usage * (1 + change[i] / 100.0));
+                usage = usage * change[i] / 100;
+                // total 사용량에 더하기
+                total_usage += usage;
+                // total 사용량이 더 많으면 n개월 리턴
+                if(total_usage > storage){
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+}
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Water {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] change = {10, -10, 10, -10, 10, -10, 10, -10, 10, -10};
+        int storage;
+        int usage;
+        System.out.println("Storage: ");
+        storage = sc.nextInt();
+        System.out.println("usage: ");
+        usage = sc.nextInt();
+        System.out.println(Arrays.toString(change));
         System.out.println("결과: " + solution(change,storage,usage));
         sc.close();
     }
+
+    // 저장량에 사용량 뺄셈
+    // 매달 물 사용량을 정확하게 나타내고 싶어서 double
     static int solution(int[] change, int storage, int usage){
         double a = storage;
         double b = usage;
@@ -23,7 +63,7 @@ public class Water {
                 b = b * (change[i] + 100.0) / 100;
 
                 if(b > a){
-                    return i+1;
+                    return i;
                 }
                 a -= b;
             }
